23418040b885bad209282ff302c5526ed1ffa292
Emrick <emrick.rivet@ynov.com>
2026-05-27 11:46:54 +0200

    (front)(add) Ajout des boites d'amélioration, et des amelioration caché avec scroll possible.

--- app/vue/jeu.js
+++ app/vue/jeu.js
@@ -2,14 +2,27 @@
 const vue = {
     //Méthode qui met à jour le score dans l'affichage
     updateScore(nouveauScore) {
-        const affichageScore = document.getElementById("nbClics")
+        const affichageScore = document.getElementById("nbScore")
         affichageScore.textContent = "Nombre de clics : " + nouveauScore
         console.log("Nombre de clics : " + nouveauScore)
     },
 
+    //Méthode qui met à jour l'argent' dans l'affichage
+    updateArgent(nouveauArgent) {
+        const affichageArgent = document.getElementById("argent")
+        affichageArgent.textContent = "Nombre de clics : " + nouveauArgent
+        console.log(nouveauArgent + "argents")
+    },
+
+    //Méthode qui met à jour les améliorations dans l'affichage selon l'argent disponible
+
+    //Méthode qui met à jour les succès dans l'affichage selon les succès débloqués par le joueur
+
+    //Méthode qui met à jour les paliers dans l'affichage selon les paliers débloqués par le joueur
+
     updateFish(nouveauPoisson) {
-        const afficagePoisson = document.getElementById("poisson")
-        afficagePoisson.textContent = "Poisson " + nouveauPoisson
+        const affichagePoisson = document.getElementById("nomPoisson")
+        affichagePoisson.textContent = "Poisson " + nouveauPoisson
     }
 }
 
