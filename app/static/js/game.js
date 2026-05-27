44c371cde18cab0533e2b8f71928e8716aa9e18a
Emrick <emrick.rivet@ynov.com>
2026-05-27 12:38:59 +0200

    (front)(add) Ajout du bouton option avec son modale.

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -2,3 +2,11 @@ setInterval(function () {
     controller.sauvegarderPartie()
     console.log("Sauvegarde effectuée")
 }, 5000);
+
+document.getElementById('options').addEventListener('click', () => {
+    document.getElementById('modale-options').classList.remove('hidden')
+})
+
+document.getElementById('fermer-options').addEventListener('click', () => {
+    document.getElementById('modale-options').classList.add('hidden')
+})
